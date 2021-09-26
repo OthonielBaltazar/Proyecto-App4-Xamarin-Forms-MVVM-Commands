095ec2ca0084110f0bba04e123050b2dd172e548
OthonielBaltazar <2018371011@uteq.edu.mx>
2021-09-25 22:29:33 -0500

    App4

---
+++ App4/App4/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using App4.ViewModels;
+using Xamarin.Forms;
+
+namespace App4
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            CountViewModel countViewModel = new CountViewModel();
+            BindingContext = countViewModel;
+        }
+    }
+}
